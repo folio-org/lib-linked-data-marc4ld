6d5cdc08afbfb531f1e5c5c5691b919f4d3ef881
pbobylev <pavel_bobylev@epam.com>
2023-12-28 15:39:28 +0600

    MODLD-196: ControlFieldsBuilder - ensure value length

--- src/main/java/org/folio/marc4ld/service/ld2marc/resource/field/ControlFieldsBuilder.java
+++ src/main/java/org/folio/marc4ld/service/ld2marc/resource/field/ControlFieldsBuilder.java
@@ -1,6 +1,8 @@
 package org.folio.marc4ld.service.ld2marc.resource.field;
 
 import static org.apache.commons.lang3.StringUtils.SPACE;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import static org.apache.commons.lang3.StringUtils.rightPad;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,11 +17,22 @@ public class ControlFieldsBuilder {
   private final Map<String, StringBuilder> storage = new LinkedHashMap<>();
 
   public void addFieldValue(String tag, String newValue, Integer startPos, Integer endPos) {
-    storage.putIfAbsent(tag, new StringBuilder(EMPTY_CF));
-    storage.get(tag).replace(startPos - 1, endPos - 1, newValue);
+    if (isNotEmpty(newValue)) {
+      storage.putIfAbsent(tag, new StringBuilder(EMPTY_CF));
+      storage.get(tag).replace(startPos - 1, endPos - 1, ensureLength(newValue, endPos - startPos));
+    }
   }
 
   public Stream<ControlField> build(MarcFactory factory) {
     return storage.entrySet().stream().map(e -> factory.newControlField(e.getKey(), e.getValue().toString()));
+  }
+
+  private String ensureLength(String str, int size) {
+    str = str.strip();
+    if (str.length() > size) {
+      return str.substring(0, size);
+    } else {
+      return rightPad(str, size);
+    }
   }
 }
