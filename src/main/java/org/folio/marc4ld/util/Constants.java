736af5e1849e492cbead8926d31115322ff572d6
Pavel Bobylev <131748689+PBobylev@users.noreply.github.com>
2024-01-22 19:50:58 +0600

    MODLD-205: Internal ids added to ld2marc flow (#39)

---
+++ src/main/java/org/folio/marc4ld/util/Constants.java
@@ -0,0 +1,12 @@
+package org.folio.marc4ld.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+  public static final String FIELD_UUID = "999";
+  public static final char SUBFIELD_INVENTORY_ID = 'i';
+  public static final char SUBFIELD_SRS_ID = 's';
+
+}
