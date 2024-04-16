cd66ef6bf10cfb72b53fd4d0281c07835310cd7e
askhat-abishev <150008941+askhat-abishev@users.noreply.github.com>
2024-04-16 09:22:54 +0500

    MODLD-335: Convert marc field 257 to Place Resource (#80)

--- src/test/java/org/folio/marc4ld/mapper/Marc2BibframeMapperIT.java
+++ src/test/java/org/folio/marc4ld/mapper/Marc2BibframeMapperIT.java
@@ -32,6 +32,7 @@ import static org.folio.ld.dictionary.PredicateDictionary.MEDIA;
 import static org.folio.ld.dictionary.PredicateDictionary.MEDIUM;
 import static org.folio.ld.dictionary.PredicateDictionary.NARRATOR;
 import static org.folio.ld.dictionary.PredicateDictionary.ONSCREEN_PRESENTER;
+import static org.folio.ld.dictionary.PredicateDictionary.ORIGIN_PLACE;
 import static org.folio.ld.dictionary.PredicateDictionary.PATRON;
 import static org.folio.ld.dictionary.PredicateDictionary.PE_DISTRIBUTION;
 import static org.folio.ld.dictionary.PredicateDictionary.PE_MANUFACTURE;
@@ -159,6 +160,8 @@ import static org.folio.ld.dictionary.ResourceTypeDictionary.TOPIC;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.VARIANT_TITLE;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.WORK;
 import static org.folio.marc4ld.mapper.test.TestUtil.loadResourceAsString;
+import static org.folio.marc4ld.mapper.test.TestUtil.validateEdge;
+import static org.folio.marc4ld.mapper.test.TestUtil.validateProperty;
 
 import java.util.List;
 import java.util.Map;
@@ -604,6 +607,7 @@ class Marc2BibframeMapperIT {
     validateTitle3(edgeIterator.next());
     validateVariantTitle(edgeIterator.next());
     validateParallelTitle(edgeIterator.next());
+    validateEdge(edgeIterator.next(), ORIGIN_PLACE, List.of(PLACE), Map.of(NAME.getValue(), "France"), "France");
     validateCategory(edgeIterator.next(), CONTENT, "contentTypes");
     validateSubjectEdge(edgeIterator.next(), List.of(CONCEPT, FAMILY),
       getFamilyPersonConceptExpectedProperties("family"));
@@ -1111,24 +1115,6 @@ class Marc2BibframeMapperIT {
     assertThat(temporalEdge.getTarget().getOutgoingEdges()).isEmpty();
     assertThat(placeEdge.getTarget().getOutgoingEdges()).isEmpty();
     assertThat(edgeIterator.hasNext()).isFalse();
-  }
-
-  private void validateEdge(ResourceEdge edge, PredicateDictionary predicate,
-                            List<ResourceTypeDictionary> types, Map<String, String> properties, String expectedLabel) {
-    assertThat(edge.getId()).isNull();
-    assertThat(edge.getPredicate().getHash()).isEqualTo(predicate.getHash());
-    assertThat(edge.getPredicate().getUri()).isEqualTo(predicate.getUri());
-    assertThat(edge.getTarget().getId()).isNotNull();
-    assertThat(edge.getTarget().getTypes()).containsOnly(types.toArray(new ResourceTypeDictionary[0]));
-    assertThat(edge.getTarget().getLabel()).isEqualTo(expectedLabel);
-    assertThat(edge.getTarget().getDoc()).hasSize(properties.size());
-    properties.forEach((property, propertyValue) -> validateProperty(edge.getTarget(), property, propertyValue));
-  }
-
-  private void validateProperty(Resource resource, String propertyKey, String propertyValue) {
-    assertThat(resource.getDoc().has(propertyKey)).isTrue();
-    assertThat(resource.getDoc().get(propertyKey).size()).isEqualTo(1);
-    assertThat(resource.getDoc().get(propertyKey).get(0).asText()).isEqualTo(propertyValue);
   }
 
 }
