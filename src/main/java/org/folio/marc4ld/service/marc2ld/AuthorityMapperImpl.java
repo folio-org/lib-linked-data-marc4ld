64d63bbb5208d8d42b3772afaa7851ebe9746a36
Aleksei Pronichev <aleksei_pronichev@epam.com>
2024-06-03 18:28:10 +0500

    clean

--- src/main/java/org/folio/marc4ld/service/marc2ld/AuthorityMapperImpl.java
+++ src/main/java/org/folio/marc4ld/service/marc2ld/AuthorityMapperImpl.java
@@ -1,36 +1,26 @@
 package org.folio.marc4ld.service.marc2ld;
 
 import static java.lang.Character.MIN_VALUE;
-import static java.util.Objects.isNull;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.CONCEPT;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.PERSON;
-import static org.folio.marc4ld.util.BibframeUtil.isNotEmpty;
-import static org.folio.marc4ld.util.Constants.FIELD_UUID;
-import static org.folio.marc4ld.util.Constants.SUBFIELD_INVENTORY_ID;
-import static org.folio.marc4ld.util.Constants.SUBFIELD_SRS_ID;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.ld.fingerprint.service.FingerprintHashService;
 import org.folio.marc4ld.service.condition.ConditionChecker;
-import org.folio.marc4ld.service.label.LabelService;
 import org.folio.marc4ld.service.marc2ld.field.FieldController;
 import org.folio.marc4ld.service.marc2ld.preprocessor.FieldPreprocessor;
 import org.marc4j.MarcJsonReader;
 import org.marc4j.marc.ControlField;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.MarcFactory;
-import org.marc4j.marc.Subfield;
 import org.springframework.stereotype.Service;
 
 @Log4j2
@@ -44,7 +34,6 @@ public class AuthorityMapperImpl implements AuthorityMapper {
   private final ConditionChecker conditionChecker;
   private final FieldController fieldController;
   private final FingerprintHashService hashService;
-  private final LabelService labelService;
 
   @Override
   public Collection<Resource> fromAuthorityMarcJson(String marc) {
@@ -58,12 +47,10 @@ public class AuthorityMapperImpl implements AuthorityMapper {
     var reader = getReader(marc);
     while (reader.hasNext()) {
       var resource = new Resource() //TODO temporary
-        .addType(CONCEPT).addType(PERSON)
-        ;
+        .addType(CONCEPT).addType(PERSON);
       fillInstanceFields(reader.next(), resource);
 
       resource.setId(hashService.hash(resource));
-//      cleanEmptyEdges(resource);
 
       topLevelResources.add(resource);
     }
@@ -91,12 +78,6 @@ public class AuthorityMapperImpl implements AuthorityMapper {
   //TODO duplicate from original mapper
   private void fillData(org.marc4j.marc.Record marcRecord, Resource instance, DataField dataField) {
     handleField(dataField.getTag(), instance, dataField, marcRecord);
-
-    //TODO remove inventory?
-//    if (FIELD_UUID.equals(dataField.getTag())) {
-//      instance.setInventoryId(readUuid(dataField.getSubfield(SUBFIELD_INVENTORY_ID)));
-//      instance.setSrsId(readUuid(dataField.getSubfield(SUBFIELD_SRS_ID)));
-//    }
   }
 
   private void handleField(String tag, Resource instance, DataField dataField, org.marc4j.marc.Record marcRecord) {
@@ -109,33 +90,6 @@ public class AuthorityMapperImpl implements AuthorityMapper {
           .forEach(fr -> fieldController.handleField(instance, field, marcRecord.getControlFields(), fr))
       );
   }
-
-  //TODO duplicate from original mapper
-  private UUID readUuid(Subfield subfield) {
-    if (isNull(subfield) || isNull(subfield.getData())) {
-      return null;
-    }
-    var value = subfield.getData().strip();
-    try {
-      return UUID.fromString(value);
-    } catch (Exception e) {
-      log.warn("Incorrect UUID value from Marc field 999, subfield [{}]: {}", subfield.getCode(), value);
-      return null;
-    }
-  }
-
-  //TODO duplicate from original mapper
-  private void cleanEmptyEdges(Resource resource) {
-    var outEdges = resource.getOutgoingEdges().stream()
-      .map(re -> {
-        cleanEmptyEdges(re.getTarget());
-        return re;
-      })
-      .filter(re -> isNotEmpty(re.getTarget()))
-      .collect(Collectors.toCollection(LinkedHashSet::new));
-    resource.setOutgoingEdges(outEdges);
-  }
-
 
   //TODO duplicate from original mapper
   private MarcJsonReader getReader(String marc) {
