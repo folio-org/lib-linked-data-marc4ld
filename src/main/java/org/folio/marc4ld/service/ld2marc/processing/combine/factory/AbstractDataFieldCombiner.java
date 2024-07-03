45cd76268c7ccd782f4aea090a76bf9e176ce7e3
askhat-abishev <Askhat_Abishev@epam.com>
2024-07-03 17:08:05 +0500

    MODLD-422: fix remark

--- src/main/java/org/folio/marc4ld/service/ld2marc/processing/combine/factory/AbstractDataFieldCombiner.java
+++ src/main/java/org/folio/marc4ld/service/ld2marc/processing/combine/factory/AbstractDataFieldCombiner.java
@@ -64,8 +64,8 @@ abstract class AbstractDataFieldCombiner implements DataFieldCombiner {
   }
 
   private void putFieldIfPresent(char fieldTag, DataField dataField) {
-    Optional.ofNullable(dataField.getSubfields(fieldTag))
-      .ifPresent(subfields -> subfields.forEach(combinedField::addSubfield));
+    dataField.getSubfields(fieldTag)
+      .forEach(combinedField::addSubfield);
   }
 
   private void setIndicators(DataField dataField) {
