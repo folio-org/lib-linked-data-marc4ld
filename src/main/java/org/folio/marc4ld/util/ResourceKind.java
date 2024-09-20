3f3d2dc6c17ac21f8911f5fb8f0e373f7aed32fc
pkjacob <121239864+pkjacob@users.noreply.github.com>
2024-09-20 09:03:30 -0400

    MODLD-317: Add FORM resource to ResourceKind -> AUTHORITY (#173)

--- src/main/java/org/folio/marc4ld/util/ResourceKind.java
+++ src/main/java/org/folio/marc4ld/util/ResourceKind.java
@@ -2,6 +2,7 @@ package org.folio.marc4ld.util;
 
 import static org.folio.ld.dictionary.ResourceTypeDictionary.CONCEPT;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.FAMILY;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.FORM;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.INSTANCE;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.JURISDICTION;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.MEETING;
@@ -17,6 +18,6 @@ import org.folio.ld.dictionary.ResourceTypeDictionary;
 public class ResourceKind {
   public static final Set<ResourceTypeDictionary> BIBLIOGRAPHIC = Set.of(INSTANCE, WORK);
   public static final Set<ResourceTypeDictionary> AUTHORITY = Set.of(
-    CONCEPT, PERSON, FAMILY, MEETING, JURISDICTION, ORGANIZATION
+    CONCEPT, PERSON, FAMILY, MEETING, JURISDICTION, ORGANIZATION, FORM
   );
 }
