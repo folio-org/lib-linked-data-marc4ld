c217818dc462a120130c518490cb4b888f7499d0
askhat-abishev <Askhat_Abishev@epam.com>
2024-09-20 16:59:17 +0500

    MODLD-479: Convert "supplementaryContent" edge of Work to marc field 008, char 24-27 & char 31

---
+++ src/main/java/org/folio/marc4ld/service/ld2marc/mapper/custom/impl/Ld2MarcSupplementaryContentMapper.java
@@ -0,0 +1,54 @@
+package org.folio.marc4ld.service.ld2marc.mapper.custom.impl;
+
+import static org.folio.ld.dictionary.PredicateDictionary.SUPPLEMENTARY_CONTENT;
+import static org.folio.ld.dictionary.PropertyDictionary.CODE;
+import static org.folio.marc4ld.util.BibframeUtil.getOutgoingEdges;
+import static org.folio.marc4ld.util.BibframeUtil.getPropertyValue;
+import static org.folio.marc4ld.util.BibframeUtil.getWork;
+import static org.folio.marc4ld.util.Constants.TAG_008;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.folio.ld.dictionary.model.Resource;
+import org.folio.ld.dictionary.model.ResourceEdge;
+import org.folio.marc4ld.service.ld2marc.mapper.custom.Ld2MarcCustomMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ld2MarcSupplementaryContentMapper implements Ld2MarcCustomMapper {
+
+  private static final String ONE = "1";
+
+  @Override
+  public void map(Resource resource, Context context) {
+    getWork(resource)
+      .ifPresent(work -> {
+        var supplementaryContentEdges = getOutgoingEdges(work, SUPPLEMENTARY_CONTENT);
+        var nonIndexCodes = getNonIndexCodes(supplementaryContentEdges);
+        if (!nonIndexCodes.isEmpty()) {
+          context.controlFieldsBuilder().addFieldValue(TAG_008, nonIndexCodes, 24, 28);
+        }
+        if (hasIndex(supplementaryContentEdges)) {
+          context.controlFieldsBuilder().addFieldValue(TAG_008, ONE, 31, 32);
+        }
+      });
+  }
+
+  private String getNonIndexCodes(List<ResourceEdge> supplementaryContentEdges) {
+    return supplementaryContentEdges.stream()
+      .map(ResourceEdge::getTarget)
+      .map(r -> getPropertyValue(r, CODE.getValue()))
+      .flatMap(Optional::stream)
+      .filter(code -> !ONE.equals(code))
+      .collect(Collectors.joining());
+  }
+
+  private boolean hasIndex(List<ResourceEdge> edges) {
+    return edges.stream()
+      .map(ResourceEdge::getTarget)
+      .map(r -> getPropertyValue(r, CODE.getValue()))
+      .flatMap(Optional::stream)
+      .anyMatch(ONE::equals);
+  }
+}
---
+++ src/test/java/org/folio/marc4ld/service/ld2marc/mapper/custom/impl/Ld2MarcSupplementaryContentMapperTest.java
@@ -0,0 +1,98 @@
+package org.folio.marc4ld.service.ld2marc.mapper.custom.impl;
+
+import static java.util.Collections.emptyMap;
+import static org.folio.ld.dictionary.PredicateDictionary.INSTANTIATES;
+import static org.folio.ld.dictionary.PredicateDictionary.SUPPLEMENTARY_CONTENT;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.INSTANCE;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.WORK;
+import static org.folio.marc4ld.mapper.test.MonographTestUtil.createCategory;
+import static org.folio.marc4ld.mapper.test.MonographTestUtil.createCategorySet;
+import static org.folio.marc4ld.mapper.test.MonographTestUtil.createResource;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.folio.ld.dictionary.model.Resource;
+import org.folio.marc4ld.service.ld2marc.mapper.custom.Ld2MarcCustomMapper;
+import org.folio.marc4ld.service.ld2marc.resource.field.ControlFieldsBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Ld2MarcSupplementaryContentMapperTest {
+
+  Ld2MarcSupplementaryContentMapper mapper = new Ld2MarcSupplementaryContentMapper();
+
+  @ParameterizedTest
+  @MethodSource("resourceProvider")
+  void shouldNotMap(Resource resource) {
+    //given
+    var controlFieldsBuilder = mock(ControlFieldsBuilder.class);
+    var context = new Ld2MarcCustomMapper.Context(controlFieldsBuilder, List.of());
+
+    //when
+    mapper.map(resource, context);
+
+    //then
+    verifyNoInteractions(controlFieldsBuilder);
+  }
+
+  @Test
+  void shouldMap() {
+    //given
+    var resource = createResourceWithSupplementaryContents();
+    var controlFieldsBuilder = mock(ControlFieldsBuilder.class);
+    var context = new Ld2MarcCustomMapper.Context(controlFieldsBuilder, List.of());
+
+    //when
+    mapper.map(resource, context);
+
+    //then
+    verify(controlFieldsBuilder).addFieldValue("008", "b", 24, 28);
+    verify(controlFieldsBuilder).addFieldValue("008", "1", 31, 32);
+  }
+
+  private static Stream<Arguments> resourceProvider() {
+    return Stream.of(
+      Arguments.of(new Resource()),
+      Arguments.of(createResourceWithoutSupplementaryContents())
+    );
+  }
+
+  private static Resource createResourceWithoutSupplementaryContents() {
+    var work = createResource(
+      emptyMap(),
+      Set.of(WORK),
+      emptyMap()
+    );
+
+    return createResource(
+      emptyMap(),
+      Set.of(INSTANCE),
+      Map.of(INSTANTIATES, List.of(work))
+    );
+  }
+
+  private Resource createResourceWithSupplementaryContents() {
+    var categorySet = createCategorySet("http://id.loc.gov/vocabulary/msupplcont", "Supplementary Content");
+    var work = createResource(
+      emptyMap(),
+      Set.of(WORK),
+      Map.of(SUPPLEMENTARY_CONTENT, List.of(
+        createCategory("b", "http://id.loc.gov/vocabulary/msupplcont/bibliography", "bibliography", categorySet),
+        createCategory("1", "http://id.loc.gov/vocabulary/msupplcont/index", "index", categorySet)
+      ))
+    );
+
+    return createResource(
+      emptyMap(),
+      Set.of(INSTANCE),
+      Map.of(INSTANTIATES, List.of(work))
+    );
+  }
+}
