39c1a1323bc56940fbb62777a7a6f4cc891e3a9f
askhat-abishev <askhat_abishev@epam.com>
2025-01-17 20:08:34 +0500

    MODLD-643: Preservation of MARC fields that are not rendered in the UI

--- src/test/java/org/folio/marc4ld/service/marc2ld/mapper/custom/impl/AbstractBookMapperTest.java
+++ src/test/java/org/folio/marc4ld/service/marc2ld/mapper/custom/impl/AbstractBookMapperTest.java
@@ -6,13 +6,9 @@ import static org.folio.ld.dictionary.PredicateDictionary.NULL;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.CATEGORY;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.CATEGORY_SET;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.util.Set;
-import java.util.stream.Stream;
 import org.folio.ld.dictionary.PredicateDictionary;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.ld.dictionary.model.ResourceEdge;
@@ -21,12 +17,7 @@ import org.folio.marc4ld.service.label.LabelService;
 import org.folio.marc4ld.service.marc2ld.mapper.mapper.MapperHelper;
 import org.folio.spring.testing.type.UnitTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.marc4j.marc.Leader;
 import org.marc4j.marc.MarcFactory;
-import org.marc4j.marc.Record;
 
 @UnitTest
 class AbstractBookMapperTest {
@@ -35,56 +26,6 @@ class AbstractBookMapperTest {
   MapperHelper mapperHelper = mock(MapperHelper.class);
   FingerprintHashService hashService = mock(FingerprintHashService.class);
   AbstractBookMapper mapper = new TestBookMapper(labelService, mapperHelper, hashService);
-
-  static Stream<Arguments> applicableDataProvider() {
-    return Stream.of(
-      Arguments.of('a', new char[] {'a'}),
-      Arguments.of('a', new char[] {'c'}),
-      Arguments.of('a', new char[] {'d'}),
-      Arguments.of('a', new char[] {'m'}),
-      Arguments.of('t', new char[] {'a'}),
-      Arguments.of('t', new char[] {'c'}),
-      Arguments.of('t', new char[] {'d'}),
-      Arguments.of('t', new char[] {'m'})
-    );
-  }
-
-  @ParameterizedTest
-  @MethodSource("applicableDataProvider")
-  void isApplicable_shouldReturn_true(char typeOfRecord, char[] implementationDefinedValues) {
-    //given
-    var marcRecord = mock(Record.class);
-    var leader = mock(Leader.class);
-    when(marcRecord.getLeader()).thenReturn(leader);
-    when(leader.getTypeOfRecord()).thenReturn(typeOfRecord);
-    when(leader.getImplDefined1()).thenReturn(implementationDefinedValues);
-
-    //expect
-    assertTrue(mapper.isApplicable(marcRecord));
-  }
-
-  static Stream<Arguments> notApplicableDataProvider() {
-    return Stream.of(
-      Arguments.of('a', new char[] {'b'}),
-      Arguments.of('A', new char[] {'c'}),
-      Arguments.of('a', new char[] {'@'}),
-      Arguments.of('1', new char[] {'m'})
-    );
-  }
-
-  @ParameterizedTest
-  @MethodSource("notApplicableDataProvider")
-  void isApplicable_shouldReturn_false(char typeOfRecord, char[] implementationDefinedValues) {
-    //given
-    var marcRecord = mock(Record.class);
-    var leader = mock(Leader.class);
-    when(marcRecord.getLeader()).thenReturn(leader);
-    when(leader.getTypeOfRecord()).thenReturn(typeOfRecord);
-    when(leader.getImplDefined1()).thenReturn(implementationDefinedValues);
-
-    //expect
-    assertFalse(mapper.isApplicable(marcRecord));
-  }
 
   @Test
   void map() {
