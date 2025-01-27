a68b55be6b79e98275f66f0debd4dc6c19f9cce1
askhat-abishev <150008941+askhat-abishev@users.noreply.github.com>
2025-01-27 15:54:09 +0500

    MODLD-643: Preservation of MARC fields that are not rendered in the UI (#44)

---
+++ src/main/java/org/folio/marc4ld/service/marc2ld/mapper/custom/impl/RawMarcMapper.java
@@ -0,0 +1,75 @@
+package org.folio.marc4ld.service.marc2ld.mapper.custom.impl;
+
+import static org.folio.marc4ld.util.Constants.TAG_005;
+import static org.folio.marc4ld.util.Constants.TAG_775;
+import static org.folio.marc4ld.util.Constants.TAG_776;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.ld.dictionary.model.RawMarc;
+import org.folio.ld.dictionary.model.Resource;
+import org.folio.marc4ld.configuration.property.Marc4LdRules;
+import org.folio.marc4ld.service.marc2ld.mapper.custom.CustomMapper;
+import org.marc4j.MarcJsonWriter;
+import org.marc4j.marc.MarcFactory;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.VariableField;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class RawMarcMapper implements CustomMapper {
+
+  private static final Set<String> SELECTED_TAGS = Set.of(TAG_775, TAG_776);
+  private static final Set<String> EXCLUDED_TAGS = Set.of(TAG_005);
+
+  private final MarcFactory marcFactory;
+  private final Marc4LdRules marc4LdRules;
+
+  @Override
+  public boolean isApplicable(Record marcRecord) {
+    return true;
+  }
+
+  @Override
+  public void map(Record marcRecord, Resource instance) {
+    var unprocessed = marcFactory.newRecord();
+    marcRecord.getVariableFields()
+      .stream()
+      .filter(variableField -> notMapped(variableField) || isSelected(variableField))
+      .filter(this::notExcluded)
+      .forEach(unprocessed::addVariableField);
+    unprocessed.setLeader(marcRecord.getLeader());
+    toString(unprocessed)
+      .ifPresent(marcString -> instance.setUnmappedMarc(new RawMarc().setContent(marcString)));
+  }
+
+  private boolean notMapped(VariableField variableField) {
+    return !marc4LdRules.getBibFieldRules().containsKey(variableField.getTag());
+  }
+
+  private boolean isSelected(VariableField variableField) {
+    return SELECTED_TAGS.contains(variableField.getTag());
+  }
+
+  private boolean notExcluded(VariableField variableField) {
+    return !EXCLUDED_TAGS.contains(variableField.getTag());
+  }
+
+  private Optional<String> toString(Record marcRecord) {
+    try (var byteArrayOutputStream = new ByteArrayOutputStream()) {
+      var writer = new MarcJsonWriter(byteArrayOutputStream);
+      writer.write(marcRecord);
+      writer.close();
+      return Optional.of(byteArrayOutputStream.toString());
+    } catch (IOException e) {
+      log.error("Exception during marc to resource conversion", e);
+      return Optional.empty();
+    }
+  }
+}
---
+++ src/test/java/org/folio/marc4ld/service/marc2ld/mapper/custom/impl/RawMarcMapperTest.java
@@ -0,0 +1,84 @@
+package org.folio.marc4ld.service.marc2ld.mapper.custom.impl;
+
+import static org.folio.marc4ld.mapper.test.TestUtil.loadResourceAsString;
+import static org.folio.marc4ld.util.Constants.A;
+import static org.folio.marc4ld.util.Constants.SPACE;
+import static org.folio.marc4ld.util.Constants.TAG_005;
+import static org.folio.marc4ld.util.Constants.TAG_100;
+import static org.folio.marc4ld.util.Constants.TAG_775;
+import static org.folio.marc4ld.util.Constants.TAG_776;
+import static org.folio.marc4ld.util.Constants.THREE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Map;
+import org.folio.ld.dictionary.model.Resource;
+import org.folio.marc4ld.configuration.property.Marc4LdRules;
+import org.folio.spring.testing.type.UnitTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.marc4j.marc.MarcFactory;
+import org.marc4j.marc.impl.ControlFieldImpl;
+import org.marc4j.marc.impl.DataFieldImpl;
+import org.marc4j.marc.impl.LeaderImpl;
+import org.marc4j.marc.impl.RecordImpl;
+import org.marc4j.marc.impl.SubfieldImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@UnitTest
+@ExtendWith(MockitoExtension.class)
+class RawMarcMapperTest {
+
+  @Mock
+  MarcFactory marcFactory;
+
+  @Mock
+  Marc4LdRules marc4LdRules;
+
+  @InjectMocks
+  private RawMarcMapper mapper;
+
+  @Test
+  void isApplicableShouldReturnTrue() {
+    //given
+    var marcRecord = new RecordImpl();
+
+    //expect
+    assertTrue(mapper.isApplicable(marcRecord));
+  }
+
+  @Test
+  void mapShouldMapRawData() {
+    //given
+    var marcRecord = new RecordImpl();
+    marcRecord.setLeader(new LeaderImpl("01767cam a22003977i 4500"));
+    var mappedDataField = new DataFieldImpl(TAG_100, THREE, SPACE);
+    mappedDataField.addSubfield(new SubfieldImpl(A, "Family name"));
+    var selectedDataField775 = new DataFieldImpl(TAG_775, SPACE, SPACE);
+    selectedDataField775.addSubfield(new SubfieldImpl(A, "775 data"));
+    var selectedDataField776 = new DataFieldImpl(TAG_776, SPACE, SPACE);
+    selectedDataField776.addSubfield(new SubfieldImpl(A, "776 data"));
+    var unmappedDataField = new DataFieldImpl("880", SPACE, SPACE);
+    unmappedDataField.addSubfield(new SubfieldImpl(A, "unmapped data"));
+    marcRecord.addVariableField(new ControlFieldImpl(TAG_005, "005 data"));
+    marcRecord.addVariableField(mappedDataField);
+    marcRecord.addVariableField(selectedDataField775);
+    marcRecord.addVariableField(selectedDataField776);
+    marcRecord.addVariableField(unmappedDataField);
+    var resource = new Resource();
+    when(marcFactory.newRecord()).thenReturn(new RecordImpl());
+    when(marc4LdRules.getBibFieldRules())
+      .thenReturn(Map.of(TAG_100, List.of(), TAG_775, List.of(), TAG_776, List.of()));
+    var expectedRawMarc = loadResourceAsString("raw_marc.jsonl");
+
+    //when
+    mapper.map(marcRecord, resource);
+
+    //then
+    assertEquals(expectedRawMarc, resource.getUnmappedMarc().getContent());
+  }
+}
