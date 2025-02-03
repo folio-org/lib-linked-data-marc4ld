9555a50ecb954c53ecbb35048f10e40c9b1c2390
askhat-abishev <askhat_abishev@epam.com>
2025-02-03 18:36:31 +0500

    Revert "fix merge conflict"
    This reverts commit 9abf953e57af65fa10df039fa3a97636b32b36f9.

--- src/test/java/org/folio/marc4ld/mapper/field500/Ld2Marc500IT.java
+++ src/test/java/org/folio/marc4ld/mapper/field500/Ld2Marc500IT.java
@@ -14,11 +14,9 @@ import org.folio.ld.dictionary.PredicateDictionary;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.marc4ld.mapper.test.MonographTestUtil;
 import org.folio.marc4ld.mapper.test.SpringTestConfig;
-import org.folio.marc4ld.service.ld2marc.Ld2MarcMapper;
-import org.folio.marc4ld.service.ld2marc.impl.Ld2MarcUnitedMapper;
+import org.folio.marc4ld.service.ld2marc.Ld2MarcMapperImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -27,8 +25,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Ld2Marc500IT {
 
   @Autowired
-  @Qualifier(Ld2MarcUnitedMapper.NAME)
-  private Ld2MarcMapper ld2MarcMapper;
+  private Ld2MarcMapperImpl ld2MarcMapper;
 
   @Test
   void shouldMapField500() {
