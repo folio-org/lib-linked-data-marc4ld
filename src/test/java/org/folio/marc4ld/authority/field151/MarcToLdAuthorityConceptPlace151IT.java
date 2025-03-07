d8b25e6d319ae64f4318118aab40c945b8f632b1
PBobylev <pavel_bobylev@epam.com>
2025-03-07 16:56:36 +0500

    MODLD-530: field 151 -> Concept, Place mapping

--- src/test/java/org/folio/marc4ld/authority/field151/MarcToLdAuthorityConceptPlace151IT.java
+++ src/test/java/org/folio/marc4ld/authority/field151/MarcToLdAuthorityConceptPlace151IT.java
@@ -1,17 +1,16 @@
-package org.folio.marc4ld.authority.field111;
+package org.folio.marc4ld.authority.field151;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.ld.dictionary.PropertyDictionary.CHRONOLOGICAL_SUBDIVISION;
-import static org.folio.ld.dictionary.PropertyDictionary.DATE;
 import static org.folio.ld.dictionary.PropertyDictionary.FORM_SUBDIVISION;
 import static org.folio.ld.dictionary.PropertyDictionary.GENERAL_SUBDIVISION;
 import static org.folio.ld.dictionary.PropertyDictionary.GEOGRAPHIC_SUBDIVISION;
 import static org.folio.ld.dictionary.PropertyDictionary.LABEL;
+import static org.folio.ld.dictionary.PropertyDictionary.MISC_INFO;
 import static org.folio.ld.dictionary.PropertyDictionary.NAME;
 import static org.folio.ld.dictionary.PropertyDictionary.RESOURCE_PREFERRED;
-import static org.folio.ld.dictionary.PropertyDictionary.SUBORDINATE_UNIT;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.CONCEPT;
-import static org.folio.ld.dictionary.ResourceTypeDictionary.MEETING;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.PLACE;
 import static org.folio.marc4ld.mapper.test.TestUtil.loadResourceAsString;
 import static org.folio.marc4ld.mapper.test.TestUtil.validateResource;
 import static org.folio.marc4ld.test.helper.AuthorityValidationHelper.validateFocusResource;
@@ -19,24 +18,20 @@ import static org.folio.marc4ld.test.helper.AuthorityValidationHelper.validateId
 
 import java.util.List;
 import java.util.Map;
-import org.folio.ld.dictionary.PropertyDictionary;
 import org.folio.marc4ld.Marc2LdTestBase;
 import org.folio.marc4ld.test.helper.AuthorityValidationHelper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-class MarcToLdAuthorityConceptMeeting111IT extends Marc2LdTestBase {
+class MarcToLdAuthorityConceptPlace151IT extends Marc2LdTestBase {
 
-  private static final String EXPECTED_MAIN_LABEL = "aValue, dValue, cValue1, cValue2, eValue -- vValue1 -- vValue2"
-    + " -- xValue1 -- xValue2 -- yValue1 -- yValue2 -- zValue1 -- zValue2";
-  private static final String EXPECTED_FOCUS_LABEL = "aValue, dValue, cValue1, cValue2, eValue";
-  private static final String EXPECTED_MEETING_LABEL = "aValue, dValue, cValue1, cValue2, eValue";
+  private static final String EXPECTED_MAIN_LABEL = "aValue -- vValue1 -- vValue2 -- xValue1"
+    + " -- xValue2 -- yValue1 -- yValue2 -- zValue1 -- zValue2";
+  private static final String EXPECTED_FOCUS_LABEL = "aValue";
 
   @Test
-  void shouldMap111FieldCorrectly() {
+  void shouldMap151FieldCorrectly() {
     // given
-    var marc = loadResourceAsString("authority/111/marc_111_concept_meeting.jsonl");
+    var marc = loadResourceAsString("authority/151/marc_151_concept_place.jsonl");
 
     // when
     var resources = marcAuthorityToResources(marc);
@@ -46,16 +41,16 @@ class MarcToLdAuthorityConceptMeeting111IT extends Marc2LdTestBase {
       .singleElement()
       .satisfies(resource -> assertThat(resource.getOutgoingEdges()).hasSize(10))
       .satisfies(
-        resource -> validateResource(resource, List.of(CONCEPT, MEETING), generalProperties(), EXPECTED_MAIN_LABEL))
-      .satisfies(resource -> validateFocusResource(resource, MEETING, focusProperties(), EXPECTED_FOCUS_LABEL))
+        resource -> validateResource(resource, List.of(CONCEPT, PLACE), generalProperties(), EXPECTED_MAIN_LABEL))
+      .satisfies(resource -> validateFocusResource(resource, PLACE, focusProperties(), EXPECTED_FOCUS_LABEL))
       .satisfies(AuthorityValidationHelper::validateSubFocusResources)
       .satisfies(resource -> validateIdentifier(resource, "010fieldvalue"));
   }
 
   @Test
-  void shouldMap111FieldCorrectlyWhenSubfocusFieldsAreEmpty() {
+  void shouldMap151FieldCorrectlyWhenSubFocusFieldsAreEmpty() {
     // given
-    var marc = loadResourceAsString("authority/111/marc_111_meeting_empty_subfocus.jsonl");
+    var marc = loadResourceAsString("authority/151/marc_151_place_empty_subfocus.jsonl");
 
     // when
     var resources = marcAuthorityToResources(marc);
@@ -63,33 +58,16 @@ class MarcToLdAuthorityConceptMeeting111IT extends Marc2LdTestBase {
     //then
     assertThat(resources)
       .singleElement()
-      .satisfies(resource -> assertThat(resource.getOutgoingEdges()).hasSize(1))
-      .satisfies(resource -> validateResource(resource, List.of(MEETING), meetingProperties(), EXPECTED_MEETING_LABEL))
+      .satisfies(resource -> assertThat(resource.getOutgoingEdges()).hasSize(2))
+      .satisfies(resource -> validateResource(resource, List.of(CONCEPT, PLACE), shortProperties(),
+        EXPECTED_FOCUS_LABEL))
       .satisfies(resource -> validateIdentifier(resource, "010fieldvalue"));
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = {
-    "authority/111/marc_111_with_$t.jsonl",
-    "authority/111/marc_111_with_$t_empty_subfocus.jsonl"}
-  )
-  void shouldNotMap111FieldIfTitleSubfieldIsPresent(String file) {
-    // given
-    var marc = loadResourceAsString(file);
-
-    // when
-    var resources = marcAuthorityToResources(marc);
-
-    // then
-    assertThat(resources).isEmpty();
   }
 
   private Map<String, List<String>> generalProperties() {
     return Map.of(
       NAME.getValue(), List.of("aValue"),
-      PropertyDictionary.PLACE.getValue(), List.of("cValue1", "cValue2"),
-      DATE.getValue(), List.of("dValue"),
-      SUBORDINATE_UNIT.getValue(), List.of("eValue"),
+      MISC_INFO.getValue(), List.of("bValue"),
       FORM_SUBDIVISION.getValue(), List.of("vValue1", "vValue2"),
       GENERAL_SUBDIVISION.getValue(), List.of("xValue1", "xValue2"),
       CHRONOLOGICAL_SUBDIVISION.getValue(), List.of("yValue1", "yValue2"),
@@ -102,19 +80,15 @@ class MarcToLdAuthorityConceptMeeting111IT extends Marc2LdTestBase {
   private Map<String, List<String>> focusProperties() {
     return Map.of(
       NAME.getValue(), List.of("aValue"),
-      PropertyDictionary.PLACE.getValue(), List.of("cValue1", "cValue2"),
-      DATE.getValue(), List.of("dValue"),
-      SUBORDINATE_UNIT.getValue(), List.of("eValue"),
+      MISC_INFO.getValue(), List.of("bValue"),
       LABEL.getValue(), List.of(EXPECTED_FOCUS_LABEL)
     );
   }
 
-  private Map<String, List<String>> meetingProperties() {
+  private Map<String, List<String>> shortProperties() {
     return Map.of(
       NAME.getValue(), List.of("aValue"),
-      PropertyDictionary.PLACE.getValue(), List.of("cValue1", "cValue2"),
-      DATE.getValue(), List.of("dValue"),
-      SUBORDINATE_UNIT.getValue(), List.of("eValue"),
+      MISC_INFO.getValue(), List.of("bValue"),
       LABEL.getValue(), List.of(EXPECTED_FOCUS_LABEL),
       RESOURCE_PREFERRED.getValue(), List.of("true")
     );
