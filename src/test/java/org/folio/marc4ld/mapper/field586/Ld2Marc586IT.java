5b5e04a0161a65d772f22a663164079ac01fa75e
Andrei Bordak <Andrei_Bordak@epam.com>
2025-03-18 15:09:18 +0400

    MODLD-366: Convert marc field 586 (Awards Note) to Resource description

---
+++ src/test/java/org/folio/marc4ld/mapper/field586/Ld2Marc586IT.java
@@ -0,0 +1,51 @@
+package org.folio.marc4ld.mapper.field586;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.folio.ld.dictionary.PropertyDictionary.AWARDS_NOTE;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.INSTANCE;
+import static org.folio.ld.dictionary.ResourceTypeDictionary.WORK;
+import static org.folio.marc4ld.mapper.test.MonographTestUtil.createResource;
+import static org.folio.marc4ld.mapper.test.TestUtil.loadResourceAsString;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.folio.ld.dictionary.PredicateDictionary;
+import org.folio.ld.dictionary.model.Resource;
+import org.folio.marc4ld.mapper.test.SpringTestConfig;
+import org.folio.marc4ld.service.ld2marc.Ld2MarcMapperImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@EnableConfigurationProperties
+@SpringBootTest(classes = SpringTestConfig.class)
+class Ld2Marc586IT {
+
+  @Autowired
+  private Ld2MarcMapperImpl ld2MarcMapper;
+
+  @Test
+  void shouldMapField586() {
+    // given
+    var resource = createResourceWithAwardsNote();
+    var expectedMarc = loadResourceAsString("fields/586/ld2marc_586.jsonl");
+
+    // when
+    var actualMarc = ld2MarcMapper.toMarcJson(resource);
+
+    // then
+    assertThat(actualMarc).isEqualTo(expectedMarc);
+  }
+
+  private Resource createResourceWithAwardsNote() {
+    return createResource(Map.of(), Set.of(INSTANCE),
+      Map.of(PredicateDictionary.INSTANTIATES,
+        List.of(
+          createResource(
+            Map.of(AWARDS_NOTE, List.of("awards_note1", "awards_note2")),
+            Set.of(WORK),
+            Map.of()))));
+  }
+}
