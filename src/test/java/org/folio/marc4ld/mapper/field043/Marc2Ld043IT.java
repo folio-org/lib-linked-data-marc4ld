322d24bd4d79b8f96494c9fea672c6f0e8dd9114
AndreiBordak <Andrei_Bordak@epam.com>
2025-04-07 16:32:33 +0400

    MODLD-694: Geographic coverage: single "geographicCoverage" edge is created for Work which contains 043 with multiple $a fields (#79)
    * MODLD-694: Geographic coverage: single "geographicCoverage" edge is created for Work which contains 043 with multiple $a fields
    
    * MODLD-694: Update NEWS.md
    
    * MODLD-694: Remove redundant property
    
    * MODLD-694: Add NAME constant for 043 mapping

--- src/test/java/org/folio/marc4ld/mapper/field043/Marc2Ld043IT.java
+++ src/test/java/org/folio/marc4ld/mapper/field043/Marc2Ld043IT.java
@@ -1,13 +1,15 @@
 package org.folio.marc4ld.mapper.field043;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.folio.ld.dictionary.PredicateDictionary.GEOGRAPHIC_COVERAGE;
 import static org.folio.marc4ld.mapper.test.TestUtil.loadResourceAsString;
 import static org.folio.marc4ld.mapper.test.TestUtil.validateEdge;
+import static org.folio.marc4ld.util.LdUtil.getWork;
 
 import java.util.List;
 import java.util.Map;
-import org.folio.ld.dictionary.PredicateDictionary;
 import org.folio.ld.dictionary.ResourceTypeDictionary;
+import org.folio.ld.dictionary.model.Resource;
 import org.folio.marc4ld.Marc2LdTestBase;
 import org.junit.jupiter.api.Test;
 
@@ -22,23 +24,28 @@ class Marc2Ld043IT extends Marc2LdTestBase {
     var result = marcBibToResource(marc);
 
     // then
-    var work = result.getOutgoingEdges().iterator().next().getTarget();
-    assertThat(work.getOutgoingEdges())
-      .hasSize(1);
-    var resourceEdge = work.getOutgoingEdges().iterator().next();
-    validateEdge(
-      resourceEdge,
-      PredicateDictionary.GEOGRAPHIC_COVERAGE,
-      List.of(ResourceTypeDictionary.PLACE),
+    assertThat(getWork(result)).get()
+      .satisfies(this::hasGeographicCoverageEdges);
+  }
+
+  private void hasGeographicCoverageEdges(Resource work) {
+    assertThat(work.getOutgoingEdges()).hasSize(2);
+    var edgesIterator = work.getOutgoingEdges().iterator();
+    validateEdge(edgesIterator.next(), GEOGRAPHIC_COVERAGE, List.of(ResourceTypeDictionary.PLACE),
       Map.of(
         "http://bibfra.me/vocab/marc/geographicCoverage",
-        List.of("https://id.loc.gov/vocabulary/geographicAreas/n-us",
-          "https://id.loc.gov/vocabulary/geographicAreas/n-us-non-skip"),
+        List.of("https://id.loc.gov/vocabulary/geographicAreas/n-us"),
         "http://bibfra.me/vocab/lite/name", List.of("United States"),
         "http://bibfra.me/vocab/lite/label", List.of("United States"),
-        "http://bibfra.me/vocab/marc/geographicAreaCode", List.of("n-us", "n-us-non-skip")
-      ),
-      "United States"
-    );
+        "http://bibfra.me/vocab/marc/geographicAreaCode", List.of("n-us")
+      ), "United States");
+    validateEdge(edgesIterator.next(), GEOGRAPHIC_COVERAGE, List.of(ResourceTypeDictionary.PLACE),
+      Map.of(
+        "http://bibfra.me/vocab/marc/geographicCoverage",
+        List.of("https://id.loc.gov/vocabulary/geographicAreas/a-is"),
+        "http://bibfra.me/vocab/lite/name", List.of("Israel"),
+        "http://bibfra.me/vocab/lite/label", List.of("Israel"),
+        "http://bibfra.me/vocab/marc/geographicAreaCode", List.of("a-is")
+      ), "Israel");
   }
 }
