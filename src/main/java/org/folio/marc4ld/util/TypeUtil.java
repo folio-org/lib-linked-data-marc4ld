1126699a432179fcb3c12388797abd9c1bde5242
PBobylev <pavel_bobylev@epam.com>
2025-07-30 13:47:56 +0500

    MODLD-805: minor improvements

--- src/main/java/org/folio/marc4ld/util/TypeUtil.java
+++ src/main/java/org/folio/marc4ld/util/TypeUtil.java
@@ -1,12 +1,12 @@
 package org.folio.marc4ld.util;
 
+import static java.util.stream.Collectors.joining;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.BOOKS;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.CONTINUING_RESOURCES;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 import lombok.experimental.UtilityClass;
 import org.folio.ld.dictionary.ResourceTypeDictionary;
 import org.marc4j.marc.Leader;
@@ -15,24 +15,24 @@ import org.marc4j.marc.Record;
 @UtilityClass
 public class TypeUtil {
   private static final Set<TypeDefinition> TYPE_DEFINITIONS = Set.of(
-    new TypeDefinition("Book", BOOKS, MarcUtil::isBook),
-    new TypeDefinition("Serial", CONTINUING_RESOURCES, MarcUtil::isSerial)
+    new TypeDefinition(BOOKS, MarcUtil::isBook),
+    new TypeDefinition(CONTINUING_RESOURCES, MarcUtil::isSerial)
   );
 
   public static Optional<ResourceTypeDictionary> getWorkType(Record marcRecord) {
     return Optional.of(marcRecord.getLeader())
       .flatMap(leader -> TYPE_DEFINITIONS.stream()
-        .filter(th -> th.checker.apply(leader))
+        .filter(th -> th.checker.test(leader))
         .map(th -> th.type)
         .findFirst());
   }
 
   public static String getSupportedRecordTypes() {
     return TYPE_DEFINITIONS.stream()
-      .map(TypeDefinition::name)
-      .collect(Collectors.joining(", "));
+      .map(definition -> definition.type.name())
+      .collect(joining(", "));
   }
 
-  private record TypeDefinition(String name, ResourceTypeDictionary type, Function<Leader, Boolean> checker) {
+  private record TypeDefinition(ResourceTypeDictionary type, Predicate<Leader> checker) {
   }
 }
