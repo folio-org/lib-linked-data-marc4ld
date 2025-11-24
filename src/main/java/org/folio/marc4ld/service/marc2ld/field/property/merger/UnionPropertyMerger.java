f6e1eb2de7bd749cc699f3a85792fb6e51f8e727
pkjacob <121239864+pkjacob@users.noreply.github.com>
2025-11-24 08:31:18 -0500

    MODLD-886: Fix unwanted value concatenation when 'concat' config is used (#169)

--- src/main/java/org/folio/marc4ld/service/marc2ld/field/property/merger/UnionPropertyMerger.java
+++ src/main/java/org/folio/marc4ld/service/marc2ld/field/property/merger/UnionPropertyMerger.java
@@ -3,19 +3,16 @@ package org.folio.marc4ld.service.marc2ld.field.property.merger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.ListUtils;
 import org.folio.ld.dictionary.PropertyDictionary;
 import org.folio.marc4ld.service.marc2ld.field.property.Property;
-import org.folio.marc4ld.service.marc2ld.field.property.merger.function.MergeFunction;
+import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-public class PropertyMergerImpl implements PropertyMerger {
-
-  private final MergeFunction mergeFunction;
-
+@Component
+public class UnionPropertyMerger implements PropertyMerger {
   @Override
   public void merge(Map<String, List<String>> values, Property property) {
     var key = PropertyDictionary.valueOf(property.field()).getValue();
-    values.merge(key, new ArrayList<>(List.of(property.value())), mergeFunction);
+    values.merge(key, new ArrayList<>(List.of(property.value())), ListUtils::union);
   }
 }
