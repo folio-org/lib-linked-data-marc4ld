91d9ef8da700c092d6b07085a34b380395d1e947
PK Jacob <pkjacob@ebsco.com>
2026-01-27 09:25:43 -0500

    MODLD-962: Updated the comparator logic

--- src/main/java/org/folio/marc4ld/service/ld2marc/mapper/datafield/hub/HubCreatorComparator.java
+++ src/main/java/org/folio/marc4ld/service/ld2marc/mapper/datafield/hub/HubCreatorComparator.java
@@ -1,8 +1,6 @@
 package org.folio.marc4ld.service.ld2marc.mapper.datafield.hub;
 
 import static java.lang.String.CASE_INSENSITIVE_ORDER;
-import static java.util.Comparator.comparing;
-import static java.util.Comparator.nullsFirst;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.FAMILY;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.JURISDICTION;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.MEETING;
@@ -10,33 +8,34 @@ import static org.folio.ld.dictionary.ResourceTypeDictionary.ORGANIZATION;
 import static org.folio.ld.dictionary.ResourceTypeDictionary.PERSON;
 
 import java.util.Comparator;
-import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import org.folio.ld.dictionary.ResourceTypeDictionary;
 import org.folio.ld.dictionary.model.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HubCreatorComparator implements Comparator<Resource> {
-  private static final List<ResourceTypeDictionary> TYPE_ORDER = List.of(
-    PERSON, FAMILY, ORGANIZATION, JURISDICTION, MEETING
+  private static final Map<ResourceTypeDictionary, Integer> TYPE_ORDER_INDEX = Map.of(
+    PERSON, 0,
+    FAMILY, 1,
+    ORGANIZATION, 2,
+    JURISDICTION, 3,
+    MEETING, 4
   );
 
   @Override
   public int compare(Resource agent1, Resource agent2) {
-    var agent1Index = getTypeOrderIndex(agent1);
-    var agent2Index = getTypeOrderIndex(agent2);
-    if (agent1Index != agent2Index) {
-      return Integer.compare(agent1Index, agent2Index);
-    }
-    return comparing(Resource::getLabel, nullsFirst(CASE_INSENSITIVE_ORDER))
-      .compare(agent1, agent2);
+    var comparator = Comparator.comparingInt(this::getTypeOrderIndex)
+      .thenComparing(Resource::getLabel, Comparator.nullsFirst(CASE_INSENSITIVE_ORDER));
+    return comparator.compare(agent1, agent2);
   }
 
   private int getTypeOrderIndex(Resource agent) {
-    return TYPE_ORDER.stream()
-      .filter(agent::isOfType)
-      .findFirst()
-      .map(TYPE_ORDER::indexOf)
-      .orElse(TYPE_ORDER.size());
+    return agent.getTypes().stream()
+      .map(TYPE_ORDER_INDEX::get)
+      .filter(Objects::nonNull)
+      .min(Integer::compareTo)
+      .orElse(TYPE_ORDER_INDEX.size());
   }
 }
