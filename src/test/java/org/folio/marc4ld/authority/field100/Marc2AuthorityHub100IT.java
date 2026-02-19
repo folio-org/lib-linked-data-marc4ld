e8e5c7211919718f83adc2e09a2a5de164545ea3
PK Jacob <pkjacob@ebsco.com>
2026-02-19 15:21:50 -0500

    MODLD-510: Map MEDIUM back to $m

--- src/test/java/org/folio/marc4ld/authority/field100/Marc2AuthorityHub100IT.java
+++ src/test/java/org/folio/marc4ld/authority/field100/Marc2AuthorityHub100IT.java
@@ -110,9 +110,10 @@ class Marc2AuthorityHub100IT extends Marc2LdTestBase {
         "http://bibfra.me/vocab/lite/date", List.of("1917-"),
         "http://bibfra.me/vocab/library/miscInfo", List.of("British royal house,"),
         "http://bibfra.me/vocab/library/attribution", List.of("author of,"),
-        "http://bibfra.me/vocab/lite/label", List.of("Royal family, House of Windsor, Dynasty, 1917")
+        "http://bibfra.me/vocab/lite/label", List.of("Royal family, House of Windsor, Dynasty, Alt. family name, 1917"),
+        "http://bibfra.me/vocab/lite/nameAlternative", List.of("Alt. family name")
       ),
-      "Royal family, House of Windsor, Dynasty, 1917"
+      "Royal family, House of Windsor, Dynasty, Alt. family name, 1917"
     );
   }
 
@@ -130,7 +131,8 @@ class Marc2AuthorityHub100IT extends Marc2LdTestBase {
   }
 
   private void validateFamilyHub(Resource hub) {
-    var hubLabel = "Royal family, House of Windsor, Dynasty, 1917. Chronicles of the royal family book 1";
+    var hubLabel = "Royal family, House of Windsor, Dynasty, Alt. family name, 1917. "
+      + "Chronicles of the royal family book 1";
     validateResource(
       hub,
       List.of(HUB),
