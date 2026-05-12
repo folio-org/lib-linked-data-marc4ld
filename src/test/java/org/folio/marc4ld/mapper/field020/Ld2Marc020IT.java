1e1a35026461e8a83cf2acda1d2697d5a1c177a7
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2026-05-12 10:37:05 +0300

    MODLD-1058 - refactor Ld2Marc020IT and Marc2Ld020IT to @ParameterizedTest with @MethodSource, covering all ISBN mapping branches explicitly.

--- src/test/java/org/folio/marc4ld/mapper/field020/Ld2Marc020IT.java
+++ src/test/java/org/folio/marc4ld/mapper/field020/Ld2Marc020IT.java
@@ -15,10 +15,13 @@ import static org.folio.marc4ld.mapper.test.TestUtil.loadResourceAsString;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.marc4ld.mapper.test.SpringTestConfig;
 import org.folio.marc4ld.service.ld2marc.Ld2MarcMapperImpl;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,47 +33,62 @@ class Ld2Marc020IT {
   @Autowired
   private Ld2MarcMapperImpl ld2MarcMapper;
 
-  @Test
-  void should_map_each_isbn_edge() {
+  @ParameterizedTest
+  @MethodSource("isbnMappingArguments")
+  void shouldMapIsbnTo020(Resource isbn, String expectedMarcFile) {
     // given
-    var expectedMarc = loadResourceAsString("fields/020/marc_020.jsonl");
-    var resource = createInstanceWithIsbn(
-      createResource(
-        Map.of(
-          NAME, List.of("1000"),
-          QUALIFIER, List.of("hardcover")
-        ),
-        Set.of(IDENTIFIER, ID_ISBN),
-        emptyMap()
-      ).setLabel("1000"),
-      isbn(
-        Map.of(
-          NAME, List.of("1001"),
-          QUALIFIER, List.of("paperback")
-        ), "current", "1001"),
-      isbn(
-        Map.of(
-          NAME, List.of("1002"),
-          QUALIFIER, List.of("black leather")
-        ), "canceled or invalid", "1002"),
-      isbn(
-        Map.of(
-          NAME, List.of("1003")
-        ), "canceled or invalid", "1003")
-    );
+    var expectedMarc = loadResourceAsString(expectedMarcFile);
+    var instance = createInstanceWithIsbn(isbn);
 
     // when
-    var result = ld2MarcMapper.toMarcJson(resource);
+    var result = ld2MarcMapper.toMarcJson(instance);
 
     // then
     assertThat(result).isEqualTo(expectedMarc);
   }
 
-  private Resource createInstanceWithIsbn(Resource... isbns) {
+  private static Stream<Arguments> isbnMappingArguments() {
+    return Stream.of(
+      Arguments.of(
+        isbn(Map.of(NAME, List.of("1000"), QUALIFIER, List.of("hardcover")), "current", "1000"),
+        "fields/020/marc_020_current_with_qualifier.jsonl"
+      ),
+      Arguments.of(
+        isbn(Map.of(NAME, List.of("1000")), "current", "1000"),
+        "fields/020/marc_020_current_without_qualifier.jsonl"
+      ),
+      Arguments.of(
+        createResource(
+          Map.of(NAME, List.of("1000"), QUALIFIER, List.of("hardcover")),
+          Set.of(IDENTIFIER, ID_ISBN),
+          emptyMap()
+        ).setLabel("1000"),
+        "fields/020/marc_020_no_status_with_qualifier.jsonl"
+      ),
+      Arguments.of(
+        isbn(Map.of(NAME, List.of("1002"), QUALIFIER, List.of("black leather")), "canceled or invalid", "1002"),
+        "fields/020/marc_020_cancinv_with_qualifier.jsonl"
+      ),
+      Arguments.of(
+        isbn(Map.of(NAME, List.of("1003")), "canceled or invalid", "1003"),
+        "fields/020/marc_020_cancinv_without_qualifier.jsonl"
+      ),
+      Arguments.of(
+        isbn(Map.of(QUALIFIER, List.of("hardcover")), "current", ""),
+        "fields/020/marc_020_qualifier_only.jsonl"
+      ),
+      Arguments.of(
+        isbn(Map.of(NAME, List.of("1005"), QUALIFIER, List.of("paperback")), "former", "1005"),
+        "fields/020/marc_020_no_field.jsonl"
+      )
+    );
+  }
+
+  private Resource createInstanceWithIsbn(Resource isbn) {
     return createResource(
       emptyMap(),
       Set.of(INSTANCE),
-      Map.of(MAP, List.of(isbns))
+      Map.of(MAP, List.of(isbn))
     );
   }
 }
