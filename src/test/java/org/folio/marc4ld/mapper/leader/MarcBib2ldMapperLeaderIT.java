9cd148cc3ebb876b6879be58367e44d806198ad5
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2026-05-18 12:39:46 +0300

    MODLD-1059 - add parameterized test for non-monograph leader formats in MARC processing

--- src/test/java/org/folio/marc4ld/mapper/leader/MarcBib2ldMapperLeaderIT.java
+++ src/test/java/org/folio/marc4ld/mapper/leader/MarcBib2ldMapperLeaderIT.java
@@ -1,20 +1,28 @@
 package org.folio.marc4ld.mapper.leader;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.folio.marc4ld.enums.BibliographLevel.MONOGRAPH_OR_ITEM;
 import static org.folio.marc4ld.mapper.test.TestUtil.JSON_MAPPER;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
 import org.folio.marc4ld.Marc2LdTestBase;
+import org.folio.marc4ld.enums.RecordType;
 import org.folio.marc4ld.service.marc2ld.bib.MarcBib2ldMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class MarcBib2ldMapperLeaderIT extends Marc2LdTestBase {
+
+  private static final String LEADER_TEMPLATE = "      %s%s                ";
 
   @Autowired
   private MarcBib2ldMapper marcBib2ldMapper;
 
   @Test
-  void shouldProcessBook() throws Exception {
+  void shouldProcessBook() {
     // given
     var leader = "      aa                ";
 
@@ -26,7 +34,7 @@ class MarcBib2ldMapperLeaderIT extends Marc2LdTestBase {
   }
 
   @Test
-  void shouldProcessSerial() throws Exception {
+  void shouldProcessSerial() {
     // given
     var leader = "       s                ";
 
@@ -38,7 +46,7 @@ class MarcBib2ldMapperLeaderIT extends Marc2LdTestBase {
   }
 
   @Test
-  void shouldNotProcessUnknownType() throws Exception {
+  void shouldNotProcessUnknownType() {
     // given
     var leader = "                        ";
 
@@ -47,6 +55,22 @@ class MarcBib2ldMapperLeaderIT extends Marc2LdTestBase {
 
     // then
     assertThat(resource).isNotPresent();
+  }
+
+  @ParameterizedTest
+  @MethodSource("nonMonographLeaderSource")
+  void shouldNotProcess_nonMonographFormat(String leader) {
+    // when
+    var resource = marcBib2ldMapper.fromMarcJson(JSON_MAPPER.writeValueAsString(new MarcJson(leader)));
+
+    // then
+    assertThat(resource).isNotPresent();
+  }
+
+  private static Stream<String> nonMonographLeaderSource() {
+    return Arrays.stream(RecordType.values())
+      .filter(rt -> rt != RecordType.LANGUAGE_MATERIAL && rt != RecordType.MANUSCRIPT_LANGUAGE_MATERIAL)
+      .map(rt -> LEADER_TEMPLATE.formatted(rt.value, MONOGRAPH_OR_ITEM.value));
   }
 
   private record MarcJson(String leader) {
